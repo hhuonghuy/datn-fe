509da64b112ad3d802368008d7b37a1ca44b36e5
huyhhps23321 <98297761+huyhhps23321@users.noreply.github.com>
2023-12-19 21:34:19 +0700

    initial

---
+++ src/api/auth.api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const base_url = 'https://bui-huy-huong-convert-be-dangtienhung.vercel.app/api';
+
+export const authApi = {
+	register: async (data) => {
+		const response = await axios.post(`${base_url}/register`, {
+			email: data.email,
+			password: data.password,
+			role: 0,
+		});
+		return response.data;
+	},
+
+	login: async (data) => {
+		const response = await axios.post(`${base_url}/login`, {
+			email: data.email,
+			password: data.password,
+		});
+		return response.data;
+	},
+};
